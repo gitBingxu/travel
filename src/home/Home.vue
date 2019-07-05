5f810f0a48e0ab5fc3384f521cb18087d574942e
bingxu <984386667@qq.com>
2019-07-05 15:09:25 +0800

    add home

---
+++ src/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="home">welcome to home!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
